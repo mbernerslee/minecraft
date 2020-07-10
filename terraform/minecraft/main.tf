0398c3fef6aa96aef91d723dd6f9670702cd02ce
Matt <matthewbernerslee@gmail.com>
2020-07-10 16:53:13 +0000

    refining comment

--- terraform/minecraft/main.tf
+++ terraform/minecraft/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 data "aws_ami" "minecraft_ami" {
   # go to aws console (website) -> ec2 -> find instance -> AMI ID -> find the AMI ID: the part in brackets e.g/ (ami-02xxxxxxxxxxx)
-  # run:
+  # run (assuming its an aws-marketplace AMI):
   # aws ec2 describe-images --owners aws-marketplace
   # and grep the output for the AMI ID found earlier to find the below values to find the AMI
   owners = ["679593333241"]
