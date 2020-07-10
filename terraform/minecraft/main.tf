b74287b466d272e3f9b122d4c567aa6ff52435ba
Matt <matthewbernerslee@gmail.com>
2020-07-10 16:52:05 +0000

    readding terraform without all the massive terraform internal files

---
+++ terraform/minecraft/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_ami" "minecraft_ami" {
+  # go to aws console (website) -> ec2 -> find instance -> AMI ID -> find the AMI ID: the part in brackets e.g/ (ami-02xxxxxxxxxxx)
+  # run:
+  # aws ec2 describe-images --owners aws-marketplace
+  # and grep the output for the AMI ID found earlier to find the below values to find the AMI
+  owners = ["679593333241"]
+
+  filter {
+    name   = "image-id"
+    values = ["ami-029d749da014117a2"]
+  }
+}
+
+resource "aws_instance" "minecraft" {
+  ami           = data.aws_ami.minecraft_ami.id
+  instance_type = "t2.small"
+
+  tags = {
+    Name = "minecraft"
+  }
+}
