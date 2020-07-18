0e3dd3429d31be33a246ae95cc864a444cd16dd3
berners <matthewbernerslee@gmail.com>
2020-07-18 23:03:02 +0100

    stuff

--- terraform/minecraft/main.tf
+++ terraform/minecraft/main.tf
@@ -31,46 +31,57 @@ resource "aws_network_interface_sg_attachment" "minecraft" {
 }
 
 resource "aws_security_group" "minecraft" {
-  name   = "minecraft"
+  name        = "minecraft"
   description = "The security group for the minecrafter server machine"
 }
 
 resource "aws_security_group_rule" "ssh" {
-  type = "ingress"
-    security_group_id = aws_security_group.minecraft.id
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "ssh"
-    from_port = 22
-    to_port   = 22
-    protocol        = "TCP"
+  type              = "ingress"
+  security_group_id = aws_security_group.minecraft.id
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  description       = "ssh"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "TCP"
 }
 
 resource "aws_security_group_rule" "player_connections_to_minecraft_server" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.minecraft.id
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "allow minecraft server connections"
-  from_port = 25565
-  to_port   = 25565
-  protocol  = "TCP"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  description       = "allow minecraft server connections"
+  from_port         = 25565
+  to_port           = 25565
+  protocol          = "TCP"
 }
 
 resource "aws_security_group_rule" "phoenix_dev" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.minecraft.id
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "allow elixir phoenix dev on http"
-  from_port = 4000
-  to_port   = 4000
-  protocol  = "TCP"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  description       = "allow elixir phoenix dev on http"
+  from_port         = 4000
+  to_port           = 4000
+  protocol          = "TCP"
 }
 
 resource "aws_security_group_rule" "pings" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.minecraft.id
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "allow pings"
-  from_port = 8
-  to_port   = -1
-  protocol  = "icmp"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  description       = "allow pings"
+  from_port         = 8
+  to_port           = -1
+  protocol          = "icmp"
 }
+
+resource "aws_security_group_rule" "egress_to_anything" {
+  type              = "egress"
+  security_group_id = aws_security_group.minecraft.id
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  description       = "allow egress to anything"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+}
+
